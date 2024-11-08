0203e78463513489202e22a83d12985e62a955e0
harry <didgpals98@naver.com>
2024-11-08 13:21:26 +0900

    splash edit

--- js/location.js
+++ js/location.js
@@ -1,16 +1,16 @@
 $(function () {
   $(".join-form .location").on("click", function () {
-    $(".dimmd").fadeIn();
-    $(".pop-up").css({ bottom: 0 });
+    $(".dimmed").fadeIn();
+    $(".pop-up01").css({ bottom: 0 });
   });
   $(".pop-up-search ul li").on("click", function () {
     let locationValue = $(this).text();
     $(".join-form .location").val(locationValue);
-    $(".pop-up").css({ bottom: "-100%" });
-    $(".dimmd").fadeOut();
+    $(".pop-up01").css({ bottom: "-100%" });
+    $(".dimmed").fadeOut();
   });
   $(".btn-close").on("click", function () {
     $(".pop-up").css({ bottom: "-100%" });
-    $(".dimmd").fadeOut();
+    $(".dimmed01").fadeOut();
   });
 });
