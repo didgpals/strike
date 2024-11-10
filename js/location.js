6c96e75e03f43c12cc505f1162f3fc5690bd83b5
harry <didgpals98@naver.com>
2024-11-10 22:48:07 +0900

    crew-make

--- js/location.js
+++ js/location.js
@@ -1,16 +1,16 @@
 $(function () {
   $(".join-form .location").on("click", function () {
-    $(".dimmed").fadeIn();
-    $(".pop-up01").css({ bottom: 0 });
+    $("#dimmed").fadeIn();
+    $(".pop-up").css({ bottom: 0 });
   });
   $(".pop-up-search ul li").on("click", function () {
     let locationValue = $(this).text();
     $(".join-form .location").val(locationValue);
-    $(".pop-up01").css({ bottom: "-100%" });
-    $(".dimmed").fadeOut();
+    $(".pop-up").css({ bottom: "-100%" });
+    $("#dimmed").fadeOut();
   });
   $(".btn-close").on("click", function () {
     $(".pop-up").css({ bottom: "-100%" });
-    $(".dimmed01").fadeOut();
+    $("#dimmed").fadeOut();
   });
 });
