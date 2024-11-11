9b859b2e0e438a4d1dfe132c8b4ba2e17bbaa31d
harry <didgpals98@naver.com>
2024-11-11 13:28:14 +0900

    crew-regular-meeting

--- js/crew.js
+++ js/crew.js
@@ -2,6 +2,23 @@ $(function () {
   $(".tab_tag_menu li").on("click", function () {
     $(this).addClass("active").siblings().removeClass("active");
     let index = $(this).index();
-    $(".content > div").eq(index).show().siblings(".content > div").hide();
+    $(".crew .content > div")
+      .eq(index)
+      .show()
+      .siblings(".content > div")
+      .hide();
+  });
+  $(".btn_like").on("click", function (e) {
+    e.preventDefault();
+    $(".btn_like").toggleClass("active");
+  });
+  $(".tab_tag_swiper li").on("click", function () {
+    $(this).addClass("active").siblings().removeClass("active");
+    let index = $(this).index();
+    $(".crew-group-wrap .content > section ")
+      .eq(index)
+      .show()
+      .siblings(".crew-group-wrap  .content > section")
+      .hide();
   });
 });
